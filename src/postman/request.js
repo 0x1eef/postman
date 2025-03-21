35d3b8140c3ad0ce4483c8791be87e5b0ae3476e
0x1eef <0x1eef@protonmail.com>
2025-03-21 20:06:08 +0000

    fixup! postman: inherit from EventTarget

--- src/postman/request.js
+++ src/postman/request.js
@@ -1,41 +1,40 @@
 export default {
-  font(item, options = {}) {
+  font(item, _options = {}) {
     const { family, source, props } = item;
     return new FontFace(family, source, props).load();
   },
 
-  script(item, options = {}) {
-    const { href } = item;
-    return fetch(href, options)
-      .then(async res => await res.text())
-      .then(text => ({ type: "application/javascript", text }))
-      .then(props => Object.assign(document.createElement("script"), props));
-  },
-
-  css(item, options = {}) {
-    const { href } = item;
-    return fetch(href, options)
-      .then(async res => await res.text())
-      .then(text => ({ innerText: text }))
-      .then(props => Object.assign(document.createElement("style"), props));
-  },
-
-  image(item, options = {}) {
-    const { href } = item;
+  image(item, _options = {}) {
+    const { href, props } = item;
     return new Promise((resolve, reject) => {
-      const el = document.createElement("img");
+      const el = Object.assign(document.createElement("img"), props);
       el.onload = () => resolve(el);
       el.onerror = reject;
       el.src = href;
     });
   },
 
+  script(item, options = {}) {
+    const { href, props } = item;
+    return fetch(href, options)
+      .then(async res => await res.text())
+      .then(text => ({ type: "application/javascript", text, ...props }))
+      .then(props => Object.assign(document.createElement("script"), props));
+  },
+
+  css(item, options = {}) {
+    const { href, props } = item;
+    return fetch(href, options)
+      .then(async res => await res.text())
+      .then(text => ({ innerText: text, ...props }))
+      .then(props => Object.assign(document.createElement("style"), props));
+  },
+
   json(item, options = {}) {
-    const { href } = item;
+    const { href, props } = item;
     return fetch(href, options)
       .then(res => res.text())
-      .then(text => ({ type: "application/json", text }))
-      .then(props => Object.assign(props, item.props != null || {}))
+      .then(text => ({ type: "application/json", text, ...props }))
       .then(props => Object.assign(document.createElement("script"), props));
   },
 };
